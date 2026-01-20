5c1d3b4fc2a64fe40d32cbbddca4ed0d4bdb9f13
Devaji Patil <devaji.patil@arena2036.de>
2026-01-20 15:44:08 +0100

    chore:corercted typo

--- src/pages/ConsumeData.tsx
+++ src/pages/ConsumeData.tsx
@@ -601,7 +601,7 @@ export default function ConsumeData() {
             rowsPerPageOptions={[10, 25, 50, 100]}
             onSelectionModelChange={newSelectionModel => handleSelectionModel(newSelectionModel)}
             selectionModel={selectionModel}
-            isRowSelectable={params => params.row.type !== 'PCFExchangeEndpot'}
+            isRowSelectable={params => params.row.type !== 'PCFExchangeEndpoint'}
             components={{
               Toolbar: GridToolbar,
               LoadingOverlay: LinearProgress,
